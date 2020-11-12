89bb7a96e1ab607f0d524c433bad058532e34d3e
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-12 23:49:06 +0100

    implementing apiResponser on the userController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class AuthController extends ApiController
 {
 
     /**
@@ -111,11 +111,13 @@ class AuthController extends Controller
      */
     public function user(Request $request){
 
-        return response()->json($request->user());
+        $user = $request->user();
+        return $this->showOne($user);
     }
 
     public function users(){
 
-        return response()->json(User::all());
+        $users = User::all();
+        return $this->showAll($users);
     }
 }
