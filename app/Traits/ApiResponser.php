391a75e01a9d1774389d53be12ff17fab52eee73
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-12 23:35:48 +0100

    creating successResponse method for ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -8,7 +8,10 @@ use Illuminate\Support\Collection;
 
 trait ApiResponser
 {
+    private function successResponse($data, $code){
 
+        return response()->json($data, $code);
+    }
 
-
+    
 }
