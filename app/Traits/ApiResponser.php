6a9d342559501f86fa48d64174623642158fb555
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-12 23:29:06 +0100

    creating errorResponse method for ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,7 +4,15 @@
 namespace App\Traits;
 
 
+use Illuminate\Support\Collection;
+
 trait ApiResponser
 {
+
+    private function successResponse($data, $code){
+
+        return response()->json($data, $code);
+    }
+
 
 }
